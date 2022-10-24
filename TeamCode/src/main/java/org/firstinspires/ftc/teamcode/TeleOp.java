819806de0e38dd82ddb3d027e6cd65f2033df4b3
Brian Ryu <brianryu18@gmail.com>
2022-10-24 17:19:20 -0400

    test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+
+// OpMode : TeleOp, init and loop
+// LinearOpMode : runOpMode (sequential)
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="Draft Teleop")
+public class TeleOp extends OpMode {
+    /*@Override
+    public void runOpMode() throws InterruptedException {
+        // init
+        waitForStart();
+        // run
+    }*/
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Motion.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+@TeleOp(name = "Motion", group = "TeleOps")
+
+public class Motion extends LinearOpMode
+{
+    private DcMotor backLeft;
+    private DcMotor backRight;
+    private DcMotor frontRight;
+    private DcMotor frontLeft;
+
+    @Override
+    public void runOpmode() throws InterruptedException
+    {
+        backLeft = hardwareMap.dcMotor.get("BL");
+        backRight = hardwareMap.dcMotor.get("BR");
+        frontLeft = hardwareMap.dcMotor.get("FL");
+        frontRight = hardwareMap.dcMotor.get("FR");
+
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+        )
+
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+            backLeft.setPower(-gamepad1.left_stick_y);
+            backRight.setPower((-gamepad1.left_stick_y);
+            frontLeft.setPower((-gamepad1.left_stick_y);
+            frontRight.setPower((-gamepad1.left_stick_y);
+
+
+            idle();
+        }
+    }
+
+}
