22301ad7ef344fb79d39e29777674da2cfa8e8ea
cjel4 <cjel4@k12albemarle.org>
2022-12-07 17:59:31 -0500

    Revert "this might be completely broken"
    This reverts commit 812e13ae

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Motion.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Motion.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
@@ -12,24 +11,32 @@ import com.qualcomm.robotcore.util.Range;
 @TeleOp(name = "Main", group = "Mecanum Drive")
 public class Motion extends OpMode {
     private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontLeft = null;
+    private DcMotor frontRight = null;
+    private DcMotor backLeft = null;
+    private DcMotor backRight = null;
+    private DcMotor linearSlideMotor = null;
 
-    private  WheelMotion wheelClass = null;
-    private  LinearSlide linearSlideClass = null;
-    private  ClawMotion clawClass = null;
+    private Servo claw = null;
+
 
     @Override
     public void init() {
+        backLeft = hardwareMap.get(DcMotor.class, "BL");
+        backRight = hardwareMap.get(DcMotor.class, "BR");
+        frontLeft = hardwareMap.get(DcMotor.class, "FL");
+        frontRight = hardwareMap.get(DcMotor.class, "FR");
+        linearSlideMotor = hardwareMap.get(DcMotor.class, "LS");
 
-        wheelClass = new WheelMotion();
-        linearSlideClass = new LinearSlide();
-        clawClass = new ClawMotion();
+        claw = hardwareMap.get(Servo.class, "CM");
 
-        wheelClass.myInit();
-        linearSlideClass.myInit();
-        clawClass.myInit();
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        linearSlideMotor.setDirection(DcMotor.Direction.FORWARD);
 
         telemetry.addData("Status", "Initialized");
-
     }
 
     @Override
@@ -39,21 +46,218 @@ public class Motion extends OpMode {
     @Override
     public void start() {
         runtime.reset();
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        linearSlideMotor.setPower(0);
 
-        wheelClass.myStart();
-        linearSlideClass.myStart();
-        clawClass.myStart();
-
+        claw.setPosition(0);
     }
 
     @Override
     public void loop() {
 
-        wheelClass.myLoop();
-        linearSlideClass.myLoop();
-        clawClass.myLoop();
+        WheelMotors();
+        LinearSlideMotor();
+        ClawMotor();
 
         telemetry.update();
+
+    }
+
+    private int GetLinearSlideAxis(){
+
+       int output = 0;
+
+        if(gamepad2.b){
+            output--;
+        }
+        if(gamepad2.a) {
+            output++;
+        }
+
+        return output;
+    }
+
+    private void LinearSlideMotor(){
+
+        double power = GetLinearSlideAxis();
+
+        //  Slow linear slide
+        power *= 0.4;
+
+        //  SLOW FALL, not tested
+        if(gamepad2.left_bumper){
+            power = -0.0001;
+        }
+        //  Stall linear slide
+        if(gamepad2.right_bumper){
+            power = -0.1;
+        }
+
+        linearSlideMotor.setPower(power);
+
+        telemetry.addData("LinearSlide", "Linear slide is: " + (power == 0 ? "Resting" : power > 1 ? "Raising" : "Lowering"));
+
+    }
+
+    private double clawPosition = 0;
+    private double lastFrameClawAxis = 0;
+
+    private void ClawMotor(){
+
+        double clawAxis = GetClawAxis();
+
+        clawPosition += clawAxis * 0.001;
+
+        clawPosition = Range.clip(clawPosition, 0, 0.3);
+
+        //  Only change servo position if the button was pressed this frame
+        if(clawAxis != 0) {
+            claw.setPosition(clawPosition);
+        }
+
+        telemetry.addData("Claw", "Claw is: " + (clawAxis == 0 ? "Resting" : clawAxis == 1 ? "Opening" : "Closing") + " Position: " + clawPosition);
+
+        lastFrameClawAxis = clawAxis;
+
+    }
+
+    private int GetClawAxis(){
+
+        int output = 0;
+        if(gamepad2.y){
+            output++;
+        }
+        if(gamepad2.x) {
+            output--;
+        }
+
+        return -output;
+
+    }
+
+    private void WheelMotors(){
+
+        double strafeAxis = GetStrafeAxis();
+
+        //  If we're getting forward-backward input
+        if(gamepad1.left_stick_y != 0){
+           //  Forward-backward movement
+
+            ForwardBackward();
+
+            telemetry.addData("Mode", "Mode: forward-backward");
+
+        }else if(gamepad1.right_stick_x != 0){
+           //  Turning/rotating movement
+
+            Turning();
+
+            telemetry.addData("Mode", "Mode: Turning");
+
+        }if(strafeAxis != 0){
+           //  Strafing movement
+
+            Strafe(strafeAxis);
+
+            telemetry.addData("Mode", "Mode: Strafing");
+
+        }else{
+
+            StopMovement();
+
+            telemetry.addData("Mode", "Mode: Stopped");
+
+        }
+    }
+
+    private int GetStrafeAxis(){
+
+        int output = 0;
+
+        if(gamepad1.right_bumper){
+            output++;
+        }
+        if(gamepad1.left_bumper){
+            output--;
+        }
+
+        return output;
+
+    }
+
+    private void Strafe(double input){
+
+
+        double backLeftPower;
+        double backRightPower;
+        double frontLeftPower;
+        double frontRightPower;
+
+       if(input == 1){
+          // Strafe right
+
+          frontLeftPower = -1;
+          frontRightPower = 1;
+          backLeftPower = 1;
+          backRightPower = -1;
+
+       } else{
+          // Strafe left
+
+           frontLeftPower = 1;
+           frontRightPower = -1;
+           backLeftPower = -1;
+           backRightPower = 1;
+
+       }
+
+        //backLeftPower = Range.clip(drive + turn, -1.0, 1.0);
+        //backRightPower = Range.clip(drive - turn, -1.0, 1.0);
+
+        frontLeft.setPower(frontLeftPower);
+        frontRight.setPower(frontRightPower);
+        backLeft.setPower(backLeftPower);
+        backRight.setPower(backRightPower);
+
+    }
+
+    private void StopMovement(){
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+
+    }
+
+    private void Turning(){
+
+        double turnAxis = -gamepad1.right_stick_x;
+
+        frontLeft.setPower(turnAxis);
+        frontRight.setPower(-turnAxis);
+        backLeft.setPower(turnAxis);
+        backRight.setPower(-turnAxis);
+
+    }
+
+    private void ForwardBackward(){
+
+        double moveAxis = gamepad1.left_stick_y;
+        //  ??
+        //double x2 = x1*Math.cos(-Math.PI/4);
+        //double y2 = y1*Math.sin(-Math.PI/4);
+
+        //backLeftPower = Range.clip(drive + turn, -1.0, 1.0);
+        //backRightPower = Range.clip(drive - turn, -1.0, 1.0);
+
+        frontLeft.setPower(moveAxis);
+        frontRight.setPower(moveAxis);
+        backLeft.setPower(moveAxis);
+        backRight.setPower(moveAxis);
 
     }
 
